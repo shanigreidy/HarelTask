644f4b8a6492db16ea974578e84fc86eca0ff173
shanig@yad2.co.il <shanig@yad2.co.il>
2021-01-26 22:58:02 +0200

    fix login inputs bug

--- src/components/pages/login/Inputs.js
+++ src/components/pages/login/Inputs.js
@@ -3,11 +3,14 @@ import Input from '../../global/Input'
 import { inputs } from './config'
 
 export default ({ inputsFields = [] , handleChange = (e) => {e.preventDefault();} }) => {
-    const updatedInputs = inputs.map(input => {
-        if (inputsFields[input.name]) {
-            return {...input, value: inputsFields[input.name]}
+    const updatedInputs = [];
+    
+    for (const input of inputs) {
+        if (input.name in inputsFields) {
+            updatedInputs.push({...input, value: inputsFields[input.name]})
         }
-    })
+        
+    }
 
     return updatedInputs.map((input, idx) => {
         return (
