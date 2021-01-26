ef7270e85cc9efcdbefd8e572cfe1b8d70ab56a6
shanig@yad2.co.il <shanig@yad2.co.il>
2021-01-26 23:24:56 +0200

    filter dates-not finished

--- src/pages/Home/config.js
+++ src/pages/Home/config.js
@@ -64,7 +64,6 @@ export const Table = {
             },
             filterOptions: {
               logic(date, filters) {
-                date.toString();
                 if (filters[0] && filters[1]) {
                   return date < filters[0] || date > filters[1];
                 } else if (filters[0]) {
